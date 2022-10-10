e70b08c480f111d0927a6df1a76e22e8c99a73fd
ashritha369 <ashrithagowda666@gmail.com>
2022-10-10 17:32:31 +0530

    Adding current year/present year dynamically using js

---
+++ Omnifood/js/script.js
@@ -0,0 +1,6 @@
+///////////////////////////////////////////////
+// adding current year
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+yearEl.textContent = currentYear;
+////////////
