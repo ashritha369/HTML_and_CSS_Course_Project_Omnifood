ef942a0f3580d278c548379277dcb29da734b932
ashritha369 <ashrithagowda666@gmail.com>
2022-10-10 17:51:46 +0530

    Implementing Mobile Navigation using js

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -4,3 +4,13 @@ const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
 ////////////
+
+// Making mobile naviagtion-menu-icon work
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
