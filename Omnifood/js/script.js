9143fda529d6c8e958433179c4dc4112c7514e74
ashritha369 <ashrithagowda666@gmail.com>
2022-10-11 14:32:11 +0530

    Implementing Sticky Navigation bar

--- Omnifood/js/script.js
+++ Omnifood/js/script.js
@@ -47,3 +47,31 @@ allLinks.forEach(function (link) {
 
 // Smooth scrolling-on safari browser, add this below link in script tag inside index.html
 // smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
+
+// STICKY NAVIGATION
+// Here we should look into interection observer
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //In the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+
+obs.observe(sectionHeroEl);
